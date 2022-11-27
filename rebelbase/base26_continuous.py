0c065b5bdb99ddfaf282c975a58bf74d1fe2e287
Cariad Eccleston <cariad@cariad.earth>
2022-11-27 18:13:19 +0000

    Add Base26Continuous class

---
+++ rebelbase/base26_continuous.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from rebelbase.number import Number
+
+
+class Base26Continuous(Number):
+    """
+    A base 26 system that supports continuous sequences; for example: A, B,
+    C...X, Y, Z, AA, AB, AC...ZX, ZY, ZZ, AAA, AAB...
+
+    To facilitate continuous sequences, this system does not support zeros. "A"
+    represents the decimal value 1.
+    """
+
+    @classmethod
+    def digits(cls) -> tuple[Any, ...]:
+        """
+        Gets the digits of this numeric system in ascending value.
+        """
+
+        # This system doesn't support zeros; the first digit is a placeholder.
+        return tuple([None, *tuple(chr(n) for n in range(65, 65 + 26))])
+
+    @classmethod
+    def can_represent_zero(cls) -> bool:
+        """
+        Indicates whether or not this numeric system can represent zero.
+        """
+
+        # To facilitate continuous sequences, this system cannot support zeros.
+        #
+        # For example, if "A" represented zero then "AA" couldn't follow "Z".
+        # Instead, "Z" represents 26 and "AA" represents 27.
+        return False
